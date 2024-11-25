5e222a88fe05ee4e73e399f7836895fbd80cd662
Govind <govind.kalyana@gmail.com>
2024-11-25 18:52:01 +0000

    Clearing stub scaffolding check in

---
+++ src/main/java/com/gimpact/clearingstub/ClearingstubApplication.java
@@ -0,0 +1,13 @@
+package com.gimpact.clearingstub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearingstubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearingstubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gimpact/clearingstub/ClearingstubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimpact.clearingstub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearingstubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
